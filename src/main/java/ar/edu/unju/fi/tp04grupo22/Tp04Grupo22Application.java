08f810b5f1970816431dbd580760a186ffaaa164
Sebas <sebas.654@gmail.com>
2022-04-27 23:10:47 -0300

    inicio Proyecto

---
+++ src/main/java/ar/edu/unju/fi/tp04grupo22/Tp04Grupo22Application.java
@@ -0,0 +1,13 @@
+package ar.edu.unju.fi.tp04grupo22;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Tp04Grupo22Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Tp04Grupo22Application.class, args);
+	}
+
+}
